8be2301c5009be161aec2b0d9af9bca26a692fbf
Parikhit <parikhitritzz@gmail.com>
2024-01-04 18:34:31 +0530

    adding planets data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const { parse } = require('csv-parse');
 const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
 const habitablePlanets = [];
 
@@ -12,23 +13,32 @@ const isHabitablePlanet = (planet) => {
     );
 };
 
-fs.createReadStream('kepler_data.csv')
-    .pipe(
-        parse({
-            comment: '#',
-            columns: true,
-        })
-    )
-    .on('data', (data) => {
-        if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
-        }
-    })
-    .on('error', (err) => console.log(err))
-    .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(
+                parse({
+                    comment: '#',
+                    columns: true,
+                })
+            )
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err);
+            })
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} habitable planets found!`);
+                resolve();
+            });
     });
+}
 
 module.exports = {
+    loadPlanetsData,
     planets: habitablePlanets,
 };
