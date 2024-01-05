b5ca845877768e3c92a7b6c1a4af2b82208fc87b
Parikhit <parikhitritzz@gmail.com>
2024-01-05 17:28:12 +0530

    Automating project with npm

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,6 @@
+/* This code is setting up a basic Express server in JavaScript. */
+/* These lines of code are importing necessary modules for the Express server. */
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -11,6 +14,11 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
