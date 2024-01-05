c05583e6d68f68733a880d49ed0c214ce591e6bd
Parikhit <parikhitritzz@gmail.com>
2024-01-05 23:22:54 +0530

    Adding GET launches adn serving with client side routing

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -5,9 +5,11 @@ async function httpGetPlanets() {
     return response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-    // TODO: Once API is ready.
-    // Load launches, sort by flight number, and return as JSON.
+    const response = await fetch(`${API_URL}/launches`);
+    const fetchedLaunches = await response.json();
+    return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
 async function httpSubmitLaunch(launch) {
--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -13,11 +15,16 @@ app.use(
         origin: 'http://localhost:3000',
     })
 );
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
