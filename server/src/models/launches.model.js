3eb7734005d266868c984a396b6ed9bd1c335a4b
Parikhit <parikhitritzz@gmail.com>
2024-04-04 22:05:42 +0530

    Adding mongodb to project

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,6 @@
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -8,19 +11,40 @@ const launch = {
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     target: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
+    customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
 
-function getAllLaunches() {
-    return Array.from(launches.values());
+async function getAllLaunches() {
+    return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+}
+async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    });
+
+    if (!planet) {
+        throw new Error('No matching planet found!');
+    }
+
+    await launchesDatabase.updateOne(
+        {
+            flightNumber: launch.flightNumber,
+        },
+
+        launch,
+
+        {
+            upsert: true,
+        }
+    );
 }
 
 function addNewLaunch(launch) {
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,18 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+    'mongodb+srv://parikhitbaruah97:jiSuSU4LPoLtq92T@nasa-api.va46vzk.mongodb.net/?retryWrites=true&w=majority&appName=nasa-api';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => console.log('MonogDB connection ready!'));
+
+mongoose.connection.on('error', (err) => console.error(err));
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetsData();
 }
 
