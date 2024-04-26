82ee39eca1d3d85463d369fbdde25c557d35256e
Parikhit <parikhitritzz@gmail.com>
2024-04-26 15:34:26 +0530

    adding url

--- server/src/app.js
+++ server/src/app.js
@@ -16,12 +16,12 @@ app.use(
 app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/v1', api);
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
 
 module.exports = app;
